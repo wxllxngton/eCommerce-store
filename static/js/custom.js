4c7b427779daee9d91bce3df0c2ad1a1e883380f
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2022-12-26 13:58:06 +0300

    Add files via upload
    Script for loader

---
+++ static/js/custom.js
@@ -0,0 +1,26 @@
+/* ----------------------------------------------------------- */
+/*  LOADER
+  /* ----------------------------------------------------------- */
+
+var loader;
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function () {
+            loadNow(opacity - 0.18);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
